19eaef835d7b5e791d7c46304de1935c73e94504
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2022-03-31 21:59:21 +0200

    select images

--- amazon-crawler/src/product.rs
+++ amazon-crawler/src/product.rs
@@ -25,20 +25,26 @@ impl Product
 {
     pub fn from(element: ElementRef) -> Option<Self>{
 
-
+        // List of css selectors
         let title_selector = Selector::parse("h2>a>span").unwrap();
         let price = Selector::parse("span[class='a-price-whole']").unwrap();
 
         let review_selector = Selector::parse("a>i>span").unwrap();
+        let images_selector = Selector::parse("img[class='s-image']").unwrap();
+        
+        // Select the id of the product
         let id = match element.value().attr("data-asin"){
             Some(id) => id.to_string(),
             None => return None,
         };
         
+        // Select the title
         let title = match element.select(&title_selector).next() {
             Some(value) => value.inner_html(),
             None => return None,
         };
+
+        // Select the price
         let price: f32 = match element.select(&price).next() {
             Some(v) => {
                 if let Ok(v2) = v.inner_html().replace(',', ".").parse(){
@@ -50,15 +56,17 @@ impl Product
             None => return None,
         };
 
+        // Select review average
         let review = match element.select(&review_selector).next() {
             Some(v) => {
                
                 let v4 = v.inner_html().replace(',', ".");
                 match v4[0..3].parse::<f32>() {
-                    Ok(v) => Some(v/5.0),
+                    Ok(v) => {
+                        Some(v/5.0)
+                    },
                     Err(_) => None,
-                }   
-                                 
+                }                
             },
             None => None,
         };
@@ -69,7 +77,7 @@ impl Product
             title,
             price,
             platform: Plarform::Amazon,
-            images_url: vec![],
+            images_url,
             review,
             nb_review: None,
         };
